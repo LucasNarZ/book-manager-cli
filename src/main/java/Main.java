f723552162c44d02d2666f3fea9e4fecb47be32f
lucas <lucasnarloch123@gmail.com>
2026-06-10 10:09:36 -0300

    fix: instantiate userService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import repositories.BookRepository;
 import repositories.InMemoryBookRepository;
+import repositories.InMemoryUserRepository;
+import repositories.UserRepository;
 import services.BookService;
+import services.UserService;
 import ui.Menu;
 
 public class Main {
@@ -8,7 +11,10 @@ public class Main {
         BookRepository bookRepository = new InMemoryBookRepository();
         BookService bookService = new BookService(bookRepository);
 
-        Menu menu = new Menu(bookService);
+        UserRepository userRepository = new InMemoryUserRepository();
+        UserService userService = new UserService(userRepository);
+
+        Menu menu = new Menu(bookService, userService);
         menu.begin();
     }
 }
