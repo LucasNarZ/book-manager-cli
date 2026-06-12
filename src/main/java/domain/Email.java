3cc626bbcfcba8be279e22d9aa7aa5a3d1beb640
lucas <lucasnarloch123@gmail.com>
2026-06-12 19:12:00 -0300

    fix: throw correct exception

--- src/main/java/domain/Email.java
+++ src/main/java/domain/Email.java
@@ -1,15 +1,17 @@
 package domain;
 
+import exceptions.InvalidEmailException;
+
 public class Email {
     private final String value;
 
     public Email(String value) {
         if (value == null || value.isBlank()) {
-            throw new IllegalArgumentException("Email é obrigatório.");
+            throw new InvalidEmailException();
         }
 
         if (!isValid(value)) {
-            throw new IllegalArgumentException("Email inválido.");
+            throw new InvalidEmailException();
         }
 
         this.value = value.trim().toLowerCase();
